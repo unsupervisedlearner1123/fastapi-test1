a17b7e77dc9c76da2706443e4cb8e7010ad3c3f1
EC2 Default User <ec2-user@ip-172-31-24-27.us-east-2.compute.internal>
2021-09-10 13:46:49 +0000

    Updated test_main.py

--- test_main.py
+++ test_main.py
@@ -8,6 +8,6 @@ client = TestClient(app)
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Heads!"} or response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Tails!"}
+    assert response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "HEADS!"} or response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "TAILS!"}
     
 test_main()
