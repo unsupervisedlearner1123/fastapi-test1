1d0b4e21135be27b26572de718b5b871445a680e
Deekshita Saikia <deekshita.saikia@duke.edu>
2021-09-10 00:27:33 +0000

    Added pytest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 import uvicorn
 import random
 
@@ -8,6 +9,13 @@ app = FastAPI()
 async def root():
     result = random.choice(["Heads!", "Tails!"])
     return {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": result}
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Heads!"} or response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Tails!"}
     
 if __name__=="__main__":
     uvicorn.run(app,port=8080, host='0.0.0.0')
