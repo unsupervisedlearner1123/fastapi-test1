4727c76bc3213cad6cbb1029a9c26882fbf8c188
EC2 Default User <ec2-user@ip-172-31-22-87.us-east-2.compute.internal>
2021-10-08 20:57:03 +0000

    Removed unnecessary comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-# from fastapi.testclient import TestClient
 import uvicorn
 import random
 import pytest
@@ -11,12 +10,5 @@ async def root():
     result = random.choice(["HEADS!", "TAILS!"])
     return {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": result}
 
-# client = TestClient(app)
-
-# def test_root():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Heads!"} or response.json() == {"Message":"hELLO, wELCOME tO cOINtOSS", "Drumroll...it's :": "Tails!"}
-    
 if __name__=="__main__":
     uvicorn.run(app,port=8080, host='0.0.0.0')
